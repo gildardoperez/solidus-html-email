93bc1a3cf04519d350e4accbbaaadfbf6301ebc8
Weliton Freitas <notilew@gmail.com>
2015-12-22 15:44:16 -0300

    Delete base_helper_decorator.rb
    fix helper decorator

--- app/helper/base_helper_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::BaseHelper.module_eval do
-  def gem_available?(name)
-    Gem::Specification.find_by_name(name)
-  rescue Gem::LoadError
-    false
-  rescue
-    Gem.available?(name)
-  end
-end
